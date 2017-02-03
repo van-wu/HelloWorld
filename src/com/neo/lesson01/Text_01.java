ce5f344f8e4ca921ae86a2f3ccde14a0d2a47b7e
neo.wu <neo.wu@hotmail.com>
2017-02-03 16:06:28 +0800

    添加String用法 Signed-off-by: neo.wu <neo.wu@hotmail.com>

---
+++ src/com/neo/lesson01/Text_01.java
@@ -0,0 +1,12 @@
+package com.neo.lesson01;
+
+public class Text_01 {
+	static String name = "Van-wu";
+	static String phonenumber = "18271582309";
+	public static void main(String[] args) {
+		System.out.print(name);
+		System.out.println();
+		System.out.print(phonenumber);
+	}
+
+}
